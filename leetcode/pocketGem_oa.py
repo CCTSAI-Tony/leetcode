cd56db8e124a43312c036a291d93655bcc5e6120
CHIH CHIN, TSAI <o660086@tamu.edu>
2020-12-03 11:27:58 -0600

    update daily

---
+++ leetcode/pocketGem_oa.py
@@ -0,0 +1,48 @@
+leetcode: 322, 221, 124, 428, 235, 547
+
+from collections import defaultdict
+def bestTrio(friend_nodes, friends_edges, friends_from, friends_to):
+    graph = defaultdict(set)
+    for f, t in zip(friends_from, friends_to):
+        graph[f].add(t)
+        graph[t].add(f)
+        trios = set()
+        for i in range(1, friend_nodes + 1):
+            for f1 in graph[i]:
+                for f2 in graph[f1]:
+                    if i in graph[f2]:
+                        group = sorted([i, f1, f2])
+                        trios.add(tuple(group))
+    if not trios:
+        return -1
+    score = 0
+    for f1, f2, f3 in trios:
+        degree = 0
+        for f in [f1, f2, f3]:
+            degree += len(graph[f])
+        degree -= 6
+        score += degree
+    return score
+                
+        
+        
+bestTrio(6, 6, [1,2,2,3,4,5], [2,4,5,5,5,6])       
+
+
+def waysToSum(n: int, k: int) -> int:
+    mod = 10**9 + 7
+    coins = [i for i in range(1, k + 1)]
+
+    f = [1] + [0] * n
+    for coin in coins:
+        for i in range(coin, n + 1):
+            f[i] += f[i - coin]
+    return f[n] % mod
+
+    
+waysToSum(5, 3)
+
+
+
+
+
