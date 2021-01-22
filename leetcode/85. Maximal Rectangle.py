a59a6d55e4a88fcd48f833693341bdef8a219964
CHIH CHIN, TSAI <o660086@tamu.edu>
2021-01-21 22:33:03 -0600

    update daily

--- leetcode/85. Maximal Rectangle.py
+++ leetcode/85. Maximal Rectangle.py
@@ -35,4 +35,58 @@ cols == matrix.length
 matrix[i][j] is '0' or '1'.
 
 '''
+# The solution is based on largest rectangle in histogram solution. Every row in the matrix is viewed as the ground with some buildings on it. 
+# The building height is the count of consecutive 1s from that row to above rows. The rest is then the same as this solution for largest rectangle in histogram
+
+#刷題用這個, time complexity O(mn), space complexity O(n), m:rows, n: cols
+#思路: leetcode 84 進階版, 然而這題有一點dp的影子, height[i] 代表該col[i] 到目前row產生連續1 的長度 => 轉化成histogram的高度 => 轉化成 leetcode 84 題目
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        n = len(matrix[0])
+        height = [0] * (n + 1)
+        ans = 0
+        for row in matrix:
+            for i in range(n):
+                height[i] = height[i] + 1 if row[i] == '1' else 0
+            stack = [-1]
+            for i in range(n + 1):
+                while height[i] < height[stack[-1]]:
+                    h = height[stack.pop()]
+                    w = i - 1 - stack[-1]
+                    ans = max(ans, h * w)
+                stack.append(i)
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
